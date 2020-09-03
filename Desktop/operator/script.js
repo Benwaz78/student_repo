32445952385982dda3d76eaf9be4a1ae89a40d2e
seggisto <anthony_uwaechia@yahoo.com>
2020-09-03 17:49:05 +0100

    javascript assignment started

---
+++ Desktop/operator/script.js
@@ -0,0 +1,47 @@
+// perimeter of a square
+// 2(length+breadth)
+var length, breadth, squarePerimeter;
+length = 2;
+breadth = 2;
+squarePerimeter = 2 * (length + breadth)
+console.log(squarePerimeter);
+
+// area of a square
+var length, breadth, sqaureArea;
+length = 2;
+breadth = 2;
+squareArea = length * breadth;
+console.log(squareArea);
+
+//comparison operators
+// true;
+// false;
+ var squarePerimeter = true;
+ var squareArea = false;
+ var squarePerimeter = false;
+ var squareArea = true;
+Boolean(squarePerimeter >squareArea) //should output true of false
+Boolean(squarePerimeter <squareArea) //should output true of false
+
+// perimeter of a square
+// 2(length+breadth)
+var length, breadth, squarePerimeter;
+length = 4;
+breadth = 2;
+squarePerimeter = 2 * (length + breadth)
+console.log(squarePerimeter);
+
+// area of a square
+var length, breadth, sqaureArea;
+length = 4;
+breadth = 2;
+squareArea = length * breadth;
+console.log(squareArea);
+
+//almighty formula
+var a =  2, b = 4, c = 10;
+
+x = (-b + document.write(Math.sqrt(b * b - 4 * a * c))) / 2 * a; 
+y = (-b - document.write(Math.sqrt(b * b - 4 * a * c))) / 2 * a; 
+console.log(x);
+console.log(y);
